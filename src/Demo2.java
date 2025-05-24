92e91ff958fe7124e12ad83496e6b8a0c3141c69
Soubhagya-ba <soubhagyabanerjee910bankura@gmail.com>
2025-05-24 16:21:18 +0530

    while loop

---
+++ src/Demo2.java
@@ -0,0 +1,10 @@
+public class Demo2 {
+
+    public static void main(String[] args) {
+        int i=0;
+        while (i<20) {
+            System.out.println("hello");
+            i++;
+        }
+    }
+}
